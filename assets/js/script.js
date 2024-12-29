18370e5020cefa5ae31e0625e9a8cdadb372dddf
Juliana Rocumback <julianarocumback@proton.me>
2024-12-28 21:44:31 -0300

    preparando para receber as imagens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,9 @@
-// MOSTRAR MENU
-
+// MENU
 const headerNav = document.getElementById('header-nav'),
       navOpen = document.getElementById('nav-open'),
       navClose = document.getElementById('nav-close')
 
 //  Abrir menu
-
 if(navOpen){
     navOpen.addEventListener('click', () => {
         headerNav.classList.add('show-menu')
@@ -13,39 +11,35 @@ if(navOpen){
 }
 
 // Fechar menu
-
 if(navClose){
     navClose.addEventListener('click', () => {
         headerNav.classList.remove('show-menu')
     })
 }
 
+// PESQUISA
 
 
-// BANNER
+
+
+
+
+
 
 
 
 
 // FILTRO
-
 const filtro = document.getElementById('filtro'),
       categoria = document.getElementById('categoria')
       
 
-// Abrir categoria
-
+// Abrir / fechar categoria
 if(filtro){
     filtro.addEventListener('click', () => {
-        categoria.classList.add('mostrar-categoria') 
+        categoria.classList.toggle('mostrar-categoria') 
     })
 }
 
-// Fechar categoria
 
-// if(filtro){
-//     filtro.addEventListener('click', () => {
-//         categoria.classList.remove('mostrar-categoria')
-//     })
-// }
 
